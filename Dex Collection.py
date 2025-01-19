5e473dcf9f71c3b5df990522220ba516d3817646
Jacob <floatingdreamspace110@gmail.com>
2025-01-18 20:31:45 -0500

    Added initial files

--- Dex Collection.py
+++ Dex Collection.py
@@ -1,12 +1,30 @@
 import subprocess
+import sqlite3
+import streamlit
+
 subprocess.check_call(["pip", "install", "requests"])
 import requests
 
-pairId = "GNfZPYyhxmvEracmrKJ87HBJy4VPMf1h5ojDPitRqeqv"
-response = requests.get(
-    "https://api.dexscreener.com/latest/dex/pairs/solana/" + pairId,
-    headers={},
-)
-data = response.json()
+page_title = "Collecting Dex Data"
+page_icon = ":seedling:"
+layout = "centered"
 
-print(data)
+streamlit.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
+
+streamlit.header(f"Welcome to SproutCast! Your Gardening Companion")
+streamlit.subheader(f"Tell us about your garden! Please enter your information below. SproutCast will use your inputs" +
+                    " along with local weather data to predict how much water you should be giving your garden " +
+                    "using machine learning!")
+with streamlit.form("input_form"):
+    col1 = streamlit.columns(1)
+    col1.text_input("Enter your Dexscreener Pair ID: ", key="pairId")
+    submitted = streamlit.form_submit_button("Submit Form")
+
+if submitted:
+    pairId = "GNfZPYyhxmvEracmrKJ87HBJy4VPMf1h5ojDPitRqeqv"
+    response = requests.get(
+        "https://api.dexscreener.com/latest/dex/pairs/solana/" + pairId,
+        headers={},
+    )
+    data = response.json()
+    print(data)
