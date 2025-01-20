1eb85b8001af81a2bf9c88310447c6b2133aa774
Jacob <floatingdreamspace110@gmail.com>
2025-01-20 14:47:59 -0500

    Added initial files

--- DexCollection.py
+++ DexCollection.py
@@ -27,8 +27,66 @@ with streamlit.form("input_form"):
             "https://api.dexscreener.com/latest/dex/pairs/solana/" + pairId,
             headers={},
         )
-        data = response.json()
-        streamlit.subheader(data["pairs"][0]["liquidity"]["usd"])
+        if response.status_code == 200:
+            data = response.json()
+
+            pairAddress = data["pairs"][0]["pairAddress"]
+            tokenAddress = data["pairs"][0]["baseToken"]["address"]
+
+            buysM5 = data["pairs"][0]["txns"]["m5"]["buys"]
+            buysH1 = data["pairs"][0]["txns"]["h1"]["buys"]
+            sellsM5 = data["pairs"][0]["txns"]["m5"]["sells"]
+            sellsH1 = data["pairs"][0]["txns"]["h1"]["sells"]
+
+            volM5 = data["pairs"][0]["volume"]["m5"]
+            volH1 = data["pairs"][0]["volume"]["h1"]
+
+            priceM5 = data["pairs"][0]["priceChange"]["m5"]
+            priceH1 = data["pairs"][0]["priceChange"]["h1"]
+
+            liquidity = data["pairs"][0]["liquidity"]["usd"]
+
+            marketCap = data["pairs"][0]["marketCap"]
+
+            paidResponse = requests.get("https://api.dexscreener.com/orders/v1/solana/" + tokenAddress, headers={},)
+            paidData = paidResponse.json()
+            paidProfile = 0
+            paidAd = 0
+            for item in paidData:
+                if item["type"] == "tokenProfile" and item["status"] == "approved":
+                    paidProfile = 1
+                elif item["type"] == "tokenAd" and item["status"] == "approved":
+                    paidAd = 1
+
+            if "socials" in data:
+                socials = 1
+            else: socials = 0
+
+            if "websites" in data:
+                websites = 1
+            else: websites = 0
+
+            if "boosts" in data:
+                boosts = data["pairs"][0]["boosts"]["active"]
+            else: boosts = 0
+
+            streamlit.subheader("Pair Address: " + pairAddress +
+                                "\nToken Address: " + tokenAddress +
+                                "\nBuys 5m: " + buysM5 +
+                                "\nBuys 1h: " + buysH1 +
+                                "\nSells 5m: " + sellsM5 +
+                                "\nSells 1h: " + sellsH1 +
+                                "\nVolume 5m: " + volM5 +
+                                "\nVolume 1h: " + volH1 +
+                                "\nPrice 5m: " + priceM5 +
+                                "\nPrice 1h: " + priceH1 +
+                                "\nLiquidity: $" + liquidity +
+                                "\nMarket Cap: $" + marketCap +
+                                "\nPaid Profile: " + paidProfile +
+                                "\nPaid Ad: " + paidAd +
+                                "\nWebsites: " + websites +
+                                "\nSocials: " + socials +
+                                "\nBoosted: " + boosts)
 
 with streamlit.form("result_form"):
     resultPair = streamlit.text_input("Enter your Dexscreener Pair ID: ", key="resultPairId")
