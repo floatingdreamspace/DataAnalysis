d135f3849e1896ca8e0407788cd36f4c5a6edead
Jacob <floatingdreamspace110@gmail.com>
2025-01-20 12:18:08 -0500

    Added initial files

--- DexCollection.py
+++ DexCollection.py
@@ -28,7 +28,7 @@ with streamlit.form("input_form"):
             headers={},
         )
         data = response.json()
-        streamlit.subheader(data["pairs"]["[liquidity"]["usd"])
+        streamlit.subheader(data["pairs"]["liquidity"][0])
 
 with streamlit.form("result_form"):
     resultPair = streamlit.text_input("Enter your Dexscreener Pair ID: ", key="resultPairId")
