97486260ccf7de808a64c1a232241270e7fec8b3
Jacob <floatingdreamspace110@gmail.com>
2025-01-25 15:12:04 -0500

    Added initial files

--- DexCollection.py
+++ DexCollection.py
@@ -4,6 +4,10 @@ import requests
 import datetime
 import pytz
 import csv
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, ConfusionMatrixDisplay
+from sklearn.model_selection import RandomizedSearchCV, train_test_split
 
 page_title = "Collecting Dex Data"
 page_icon = ":seedling:"
@@ -20,7 +24,7 @@ streamlit.subheader(f"Tell us about your garden! Please enter your information b
                     " along with local weather data to predict how much water you should be giving your garden " +
                     "using machine learning!")
 with ((streamlit.form("input_form"))):
-    pair = streamlit.text_input("Enter your Dexscreener Pair ID: ", key="pairId")
+    pair = streamlit.text_input("Enter your plant info: ", key="pairId")
     submitted = streamlit.form_submit_button("Submit Form")
 
     if submitted:
@@ -93,35 +97,27 @@ with ((streamlit.form("input_form"))):
                                     str(paidProfile) + "', '" + str(paidAd) + "', '" + str(websites) + "', '" + str(socials) + "', '" + str(boosts) + "', '" +
                                     str(pools) + "', '" + str(dayOfWeek) + "', '" + str(current_minutes) + "', 'default')")
             streamlit.subheader(command)
-            cursor.execute(command)
-            connection.commit()
-            with open('tokens.csv', 'w', newline='') as csvfile:
-                writer = csv.writer(csvfile, delimiter=',')
-                writer.writerow([str(pairAddress), str(tokenAddress), str(buysM5), str(buysH1), str(sellsM5), str(sellsH1),
-                                 str(volM5), str(volH1), str(paidProfile), str(paidAd), str(websites), str(socials), str(boosts),
-                                 str(pools), str(dayOfWeek), str(current_minutes), "defaults"])
-            cursor.execute("SELECT * FROM tokens")
-            tokens = cursor.fetchall()
-            #for row in tokens:
-            #    if row[19] == "default":
-            #        streamlit.subheader(row[0])
 
-with streamlit.form("result_form"):
-    resultPair = streamlit.text_input("Enter your Dexscreener Pair ID: ", key="resultPairId")
-    result = streamlit.radio(
-        "Select coin result:",
-        [":Success", ":Failure"],
-        captions=[
-            "Success",
-            "Failure",
-        ],
-        horizontal=True,
-    )
+            buysToSells = float(buysH1) / float(sellsH1)
+            volToLiquidity = float(volH1) / float(liquidity)
+            volToMC = float(volH1) / float(marketCap)
+            liquidityToMC = float(liquidity) / float(marketCap)
+            liquidityToBuys = float(liquidity) / float(buysH1)
+            MCToBuys = float(marketCap) / float(buysH1)
+            poolsToLiquidity = float(pools) / float(liquidity)
+            buysToVol = float(buysH1) / float(volH1)
+            tokenInfo = [buysM5, buysH1, sellsM5, sellsH1, volM5, volH1, priceM5, priceH1, liquidity, marketCap, paidProfile,
+                         paidAd, websites, socials, boosts, pools, dayOfWeek, current_minutes, buysToSells, volToLiquidity,
+                         volToMC, liquidityToMC, liquidityToBuys, MCToBuys, poolsToLiquidity, buysToVol]
 
-    submittedb = streamlit.form_submit_button("Submit Form")
-
-    if submittedb:
-        if result == ":Success":
-            streamlit.subheader("Success")
+            data_frame = pd.read_csv("university_records.csv")
+            data_frame['result'] = data_frame['result'].map({'Failure': 0, 'Success': 1})
+            X = data_frame.drop('result', axis=1)
+            y = data_frame['result']
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+            rf = RandomForestClassifier()
+            rf.fit(X_train, y_train)
+            y_pred = rf.predict(X_test)
+            streamlit.subheader(rf.predict(tokenInfo))
 
 connection.close()
