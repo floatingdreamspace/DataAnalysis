fe78684c31f0fb1ccef9030063a0eb3199f74ad4
Jacob <floatingdreamspace110@gmail.com>
2025-01-25 15:46:56 -0500

    Added initial files

--- DexCollection.py
+++ DexCollection.py
@@ -4,8 +4,6 @@ import requests
 import datetime
 import pytz
 import csv
-import os
-os.system("pip3 install scikit-learn")
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, ConfusionMatrixDisplay
