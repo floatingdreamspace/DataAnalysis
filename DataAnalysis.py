f087f4b3d2cea2caf0a2734db74233888283be60
Jacob <floatingdreamspace110@gmail.com>
2025-01-27 19:47:33 -0500

    Added initial files

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -43,7 +43,7 @@ X = data_frame.drop('result', axis=1)
 y = data_frame['result']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 #rf = RandomForestClassifier(n_estimators=10, class_weight='balanced')
-rf = RandomForestClassifier(n_estimators=10, class_weight='balanced_subsample')
+rf = RandomForestClassifier(class_weight='balanced_subsample', min_samples_split=4, max_features='log2')
 #rf = BalancedRandomForestClassifier(n_estimators=10)
 rf.fit(X_train, y_train)
 y_pred = rf.predict(X_test)
