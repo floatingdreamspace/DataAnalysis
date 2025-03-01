4c932cbb51de275e7e871c82d2b3430d1df862e5
Jacob <floatingdreamspace110@gmail.com>
2025-03-01 11:26:38 -0500

    Added initial files

--- DataAnalysisRugs.py
+++ DataAnalysisRugs.py
@@ -24,7 +24,7 @@ DBTokens = cursor.fetchall()
 tokens.append(["buysM5", "buysH1", "sellsM5", "sellsH1", "volM5", "volH1", "priceM5", "priceH1", "liquidity", "marketCap",
                  "paidProfile", "paidAd", "websites", "socials", "boosts", "pools", "buysToSells",
                  "volToLiquidity", "volToMC", "liquidityToMC", "liquidityToBuys", "MCToBuys", "poolsToLiquidity", "buysToVol",
-                 "score", "highHolder", "lowLP", "mutable", "unlockedLP", "singleHolder", "highOwnership", "result"])
+                 "priceToVol", "priceToVol5M", "score", "highHolder", "lowLP", "mutable", "unlockedLP", "singleHolder", "highOwnership", "result"])
 for row in DBTokens:
     buysToSells = float(row[3]) / float(row[5])
     volToLiquidity = float(row[7]) / float(row[10])
@@ -34,9 +34,12 @@ for row in DBTokens:
     MCToBuys = float(row[11]) / float(row[3])
     poolsToLiquidity = float(row[17]) / float(row[10])
     buysToVol = float(row[3]) / float(row[7])
+    priceToVol = float(row[9]) / float(row[7])
+    priceToVol5M = float(row[8]) / float(row[6])
     tokens.append([row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13]
                         , row[14], row[15], row[16], row[17], buysToSells,
                         volToLiquidity, volToMC, liquidityToMC, liquidityToBuys, MCToBuys, poolsToLiquidity, buysToVol,
+                        priceToVol, priceToVol5M,
                         row[20], row[21], row[22], row[23], row[24], row[25], row[26], row[27]])
 #with open('rug_data.csv', 'w', newline='') as csvfile:
 #    writer = csv.writer(csvfile)
